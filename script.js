a0a2c7ad694020c1ef1763967269c89a18041787
Diogo Augusto <diogotrescastro@gmail.com>
2021-04-16 18:14:11 -0300

    Atualizaçao do codico

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ function createStateOptions() {
   }
 }
 
-1234567891
-11/11/1111
-
-function dateValidate(data) {
+function validateData(data) {
   if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
     const day = data.substr(0, 2);
     const month = data.substr(3, 2);
@@ -24,17 +21,53 @@ function dateValidate(data) {
   return false;
 }
 
-function dataCheck() {
-  const inputDate = document.querySelector('.date');
-  let data = inputDate.value;
-  const insertData = dateValidate(data);
-  if (!insertData && data.length) {
-    inputDate.value = '';
-    alert("A data inserida está incorreta");
+function checkData() {
+  const inputData = document.querySelector('.input-data');
+  let data = inputData.value;
+  const userData = validateData(data);
+  if (!userData && data.length) {
+    inputData.value = '';
+    alert('data invalida');
     return false;
   }
-  return insertData;
+  return userData;
 }
+
+
+function renderCurriculum(event) {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const dataUser = document.querySelector('.form-data');
+    if (checkData()) {
+      const div = document.createElement('div');
+      div.className = 'div-curriculum';
+      div.innerHTML = userInput;
+      dataUser.appendChild(div);
+    }
+  }
+}
+
+const clearButton = document.querySelector('.clear-button');
+function clearFields() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea')
+  const div = document.querySelectorAll('.div-curriculum');
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+    div[index].innerText = '';
+  }
+}
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', renderCurriculum);
+clearButton.addEventListener('click', clearFields);
 
 window.onload = function () {
   createStateOptions();
