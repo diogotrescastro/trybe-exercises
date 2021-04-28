094fe07d409ae06c8f5da4d7d36048c0b0cde36b
Diogo Augusto <diogotrescastro@gmail.com>
2021-04-28 18:04:15 -0300

    Resoluçao do exercicio 5 parte II

--- bloco_7/dia_2/parte2.js
+++ bloco_7/dia_2/parte2.js
@@ -18,20 +18,15 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-
+// 1 Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 const addTurno = (objeto, chave, valor) => {
   objeto[chave] = valor;
 }
-
 addTurno(lesson2, 'turno', 'manhã');
-
 console.log(`Resultado do exercicio 1: ${Object.entries(lesson2)}`)
 
 // 2 Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-
 const listKey = (objeto) => Object.keys(objeto);
-
 console.log(`Resultado do exercicio 2: ${listKey(lesson1)}`);
 
 
@@ -40,6 +35,9 @@ const TamanhoObjeto = (objeto) =>  Object.keys(objeto).length;
 console.log(`Resultado do exercicio 3: ${TamanhoObjeto(lesson1)}`);
 
 // 4 Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
-
 const listaValores = (objeto) =>  Object.values(objeto);
-console.log(`Resultado do exercicio 3: ${listValues(lesson1)}`);
+console.log(`Resultado do exercicio 4: ${listaValores(lesson1)}`);
+
+// 5 Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+// Necessário consulta em https://app.betrybe.com/course/fundamentals/javascript-es6/javascript-es6-objects-solution
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
