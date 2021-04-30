05a85ffa708c210707d98f1196b646bef95b13cd
Diogo Augusto <diogotrescastro@gmail.com>
2021-04-30 18:29:45 -0300

    Iniciando o exercicio do dia (bloco 8, dia 1)

---
+++ bloco_8/dia_1/script.js
@@ -0,0 +1,8 @@
+const newEmployees = () => {
+  const employees = {
+    id1: // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+    id2: // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+    id3: // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+  }
+  return employees;
+};
